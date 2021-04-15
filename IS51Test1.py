1b007945cdfe14affdf98c5aebd49bfe70ff12ad
Umbasa <umbasa@Umbasas-MacBook-Air.local>
2021-04-15 14:48:07 -0700

    finished structured-english

---
+++ IS51Test1.py
@@ -0,0 +1,17 @@
+"""
+Structured-English
+The Salary Option Program is trying to compare and determine which two salary options pays better for 10 days of work.
+
+The first salary option is offering $100 per day for 10 days of work equating to $1,000.
+
+The second salary option doubles each day offering $1 the first day, $2 the second day, $4 the third day, $8 the fourth day,
+$16 the fifth day, $32 the sixth day, $64 the seventh day, $128 the eighth day, $256 the ninth day, and $512 the 
+tenth day for 10 days of work equating to $1,023.
+
+Then the sum of the first and second salary option will be compared to determine which option pays better.
+If the first salary option is better than the second salary option, then the first salary option is better.
+If the second salary option is better than the first salary option, then the second salary option is better.
+However, if the first salary option and the second salary option equal each other, then the first salary option
+and second salary option are the same. 
+
+"""
